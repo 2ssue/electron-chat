cb96866601d0aecfa92397e87486f5f50e94f72c
2ssue <e2ssue@gmail.com>
2019-11-03 15:17:42 +0900

    New Feature: 회원가입 기능 구현

--- electron-chat/src/router/Signup.js
+++ electron-chat/src/router/Signup.js
@@ -1,15 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Errors from "./Errors";
+import firebase from "firebase/app";
+import {
+  FormContainer,
+  InputContainer,
+  Input,
+  Button
+} from "./styles/common-styles";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const handleOnChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const handleOnChangePassword = e => {
+    setPassword(e.target.value);
+  };
+
+  const handleOnChangeName = e => {
+    setName(e.target.value);
+  };
+
+  const handleOnChangeAvatar = e => {
+    setAvatar(e.target.value);
+  };
+
+  const handleOnSubmit = e => {
+    let isValid = true;
+    e.preventDefault();
+
+    if (!email.length) {
+      isValid = false;
+      errors.push("Email address can't be blank");
+    }
+
+    if (!password.length) {
+      isValid = false;
+      errors.push("Password can't be blank");
+    }
+
+    if (!name.length) {
+      isValid = false;
+      errors.push("Name can't be blank");
+    }
+
+    if (!isValid) {
+      setErrors({ errors });
+      return;
+    }
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(newUser => {
+        window.location = "/rooms";
+        return newUser.updateProfile({
+          displayName: name,
+          avatar
+        });
+      })
+      .catch(err => {
+        setErrors({ errors: err.message });
+      });
+  };
+
   return (
-    <div>
-      <h2>Signup</h2>
-      <Link to="/rooms">Create new account</Link>
-      <br />
-      <Link to="/login">cancel</Link>
-      <br />
-    </div>
+    <FormContainer onSubmit={handleOnSubmit}>
+      <Errors errorMessages={errors} />
+      <InputContainer>
+        <label>Email address *</label>
+        <Input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={handleOnChangeEmail}
+        />
+      </InputContainer>
+      <InputContainer>
+        <label>Password *</label>
+        <Input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={handleOnChangePassword}
+        ></Input>
+      </InputContainer>
+      <InputContainer>
+        <label>User name *</label>
+        <Input
+          type="text"
+          placeholder="user name"
+          value={name}
+          onChange={handleOnChangeName}
+        ></Input>
+      </InputContainer>
+      <InputContainer>
+        <label>Avatar URL</label>
+        <Input
+          type="text"
+          placeholder="avatar URL"
+          value={avatar}
+          onChange={handleOnChangeAvatar}
+        ></Input>
+      </InputContainer>
+      <div>
+        <Button>Create new account</Button>
+        <Link to="/login">
+          <Button type="button">Cancel</Button>
+        </Link>
+      </div>
+    </FormContainer>
   );
 }
 
